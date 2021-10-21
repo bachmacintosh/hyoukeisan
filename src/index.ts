0bc94efc4abf3d3f5770ab048e528fdac906df5d
Collin Bachman <collin@bachman.io>
2021-10-21 12:48:23 -0400

    Add onInstall event, move sheet setup into its own method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import {WaniKaniApiSheet} from "./sheets/WaniKaniApiSheet";
 import {UIService} from "./services/UIService";
-const apiSheet = new WaniKaniApiSheet;
-const ui = new UIService;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function onInstall() {
+  setupSheets();
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function onOpen() {
+  setupSheets();
+}
+
+function setupSheets() {
+  const apiSheet = new WaniKaniApiSheet;
+  const ui = new UIService;
+
   if (apiSheet.sheet === null) {
     apiSheet.build();
     ui.askForApiKey();
