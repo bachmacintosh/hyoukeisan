7658fe806dbcb61234c93c7767c188e7c6a23c40
Collin Bachman <collin@bachman.io>
2021-10-30 21:03:49 -0400

    Add configuration

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: 'en-US',
+    title: 'Hyoukeisan',
+    description: 'WaniKani data in your Google Sheets',
+    base: '/hyoukeisan',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        repo: 'bachmacintosh/hyoukeisan',
+        docsDir: 'docs'
+    },
+})
